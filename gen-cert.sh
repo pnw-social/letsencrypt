e9d2891ed548bcfc919093c9698d68cbf05e274e
Clark Rinker <cgrinker@gmail.com>
2017-11-15 13:04:40 -0800

    Removing Mail from sans. Fixing generation bug

--- gen-cert.sh
+++ gen-cert.sh
@@ -6,5 +6,5 @@ do
   SANS="$SANS -d $i.$1";
 done
 
-certbot certonly --standalone $SANS -d $1
-DOMAIN='$1' sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem'
+certbot certonly --standalone -d $1 $SANS -d
+DOMAIN=$1 sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem'
