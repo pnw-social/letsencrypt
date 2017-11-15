f6a16d3785cde40a90ad0f71c3a8b58c672b1917
Clark Rinker <cgrinker@gmail.com>
2017-11-15 12:51:47 -0800

    Fixing script to properly concat the haproxy useable cert

--- gen-cert.sh
+++ gen-cert.sh
@@ -1,7 +1,10 @@
+
+
 SANS=""
 for i in $(cat sans.txt)
 do
-  SANS="$SANS -d $i.pnw.social";
+  SANS="$SANS -d $i.$1";
 done
 
-certbot certonly --standalone $SANS -d pnw.social
+certbot certonly --standalone $SANS -d $1
+DOMAIN='$1' sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem'
